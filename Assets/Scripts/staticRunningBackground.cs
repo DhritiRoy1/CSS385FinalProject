bd894f84646267d76cdce0f99b7f61abad254e27
ildak-systems <ildak@uw.edu>
2024-05-03 06:05:21 -0700

    brief intro scene done
    move onto game mechanic tutorial scene

---
+++ Assets/Scripts/staticRunningBackground.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class staticRunningBackground : MonoBehaviour
+{
+    private float cameraBoundX = Camera.main.transform.position.x + Camera.main.transform.position.x;
+    public float randomSpeed;
+    public float randomDepth;
+    // Start is called before the first frame update
+    void Start()
+    {
+      randomSpeed = Random.Range(2.0f, 3.0f);
+      randomDepth = Random.Range(-1.0f, 0.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // if the sprite is within the bounds of the camera, keep travelling right
+        if (6 > transform.position.x)
+        {
+            transform.Translate(Vector2.right * Time.deltaTime * randomSpeed);
+        }
+        // if not, reset sprite position to the starting point
+        else
+        {
+            transform.position = new Vector3(-5.0f, -1.5f, randomDepth);
+        }
+        
+    }
+}
---
+++ Assets/Scripts/transitionSceneTextBubbles.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using Unity.VisualScripting;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class transitionSceneTextBubbles : MonoBehaviour
+{
+    public GameObject fadeEffect;
+    public Vector3 nextButtonOffset = new Vector3(0.0f, -0.3f, 0);
+    private int index = 0;
+    public GameObject text1;
+    public GameObject text2;
+    public GameObject text3;
+    public GameObject text4;
+    
+    GameObject[] textBoxes = new GameObject[4];
+
+    private bool dialogueStart = false;
+    SpriteRenderer spriteRenderer;
+    SpriteRenderer spriteRenderer1;
+    Color color;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        spriteRenderer = fadeEffect.GetComponent<SpriteRenderer>();
+        spriteRenderer1 = GetComponent<SpriteRenderer>();
+        color.a = 0.0f;
+        spriteRenderer1.color = color;
+
+        textBoxes[0] = text1;
+        textBoxes[0].SetActive(false);
+        textBoxes[1] = text2;
+        textBoxes[1].SetActive(false);
+        textBoxes[2] = text3;
+        textBoxes[2].SetActive(false);
+        textBoxes[3] = text4;
+        textBoxes[3].SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (dialogueStart)
+        {
+            if (Input.GetMouseButtonUp(0))
+            {
+                if (index == 3)
+                {
+                    SceneManager.LoadScene("tutorialScene");
+                }
+                textBoxes[index].SetActive(false);
+                index++;
+                textBoxes[index].SetActive(true);
+                transform.position = textBoxes[index].transform.position + nextButtonOffset;
+
+            }
+        }
+
+        else if (spriteRenderer.color.a < 0.1f)
+        {
+            // start dialogue scene
+            dialogueStart = true;
+            textBoxes[index].SetActive(true);
+            color.a = 1.0f;
+            color = Color.white;
+            spriteRenderer1.color = color;
+            transform.position = textBoxes[index].transform.position + nextButtonOffset;
+        }
+    }
+}
